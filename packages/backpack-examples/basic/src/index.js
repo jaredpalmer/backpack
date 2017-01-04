68c160efc8acf7741074792eeb6c07b8b8bd4fb5
Jared Palmer <jaredpalmer>
2017-01-04 09:27:45 -0800

    Add comments to example

--- packages/backpack-examples/basic/src/index.js
+++ packages/backpack-examples/basic/src/index.js
@@ -4,13 +4,22 @@ const app = express()
 
 app.get('/', async (req, res) => {
   try {
-    const thing = await Promise.resolve({ one: 'two' })
-    return res.json({...thing, hello: 'world'})
+    const thing = await Promise.resolve({ one: 'two' }) // async/await!
+    return res.json({...thing, hello: 'world'}) // object-rest-spread!
   } catch (e) {
     return res.json({ error: e.message })
   }
 })
+const port = process.env.PORT || 3000
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`> listening on port ${3000}`)
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err)
+  }
+
+  if (__DEV__) { // webpack flags!
+    console.log('> in development')
+  }
+
+  console.log(`> listening on port ${port}`)
 })
