cdeac1157b8956b3c3d9187a9e3733551e0bad00
Eric Clemmons <eric@smarterspam.com>
2017-02-11 21:29:49 -0700

    source-map-support uses resolved path from backpack-core

--- packages/backpack-core/config/webpack.config.js
+++ packages/backpack-core/config/webpack.config.js
@@ -107,7 +107,7 @@ module.exports = (options) => {
       // top of each file using the BannerPlugin.
       new webpack.BannerPlugin({
         raw: true,
-        banner: 'require("source-map-support/register")'
+        banner: `require('${require.resolve('source-map-support')}')`
       }),
       // The FriendlyErrorsWebpackPlugin (when combined with source-maps)
       // gives Backpack its human-readable error messages.
