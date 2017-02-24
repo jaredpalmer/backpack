85a1133c285d14bc2a56f4c0d1c8eb219c094715
Michael Frohberg <michael.frohberg@gmail.com>
2017-02-24 00:38:57 -0500

    use ava's babel preset; add test

--- examples/with-ava/src/index.test.js
+++ examples/with-ava/src/index.test.js
@@ -5,3 +5,8 @@ test('sum', t => {
     const result = sum(2, 5)
     t.is(result, 7)
 })
+
+test('sum is a poorly written function', t => {
+    const result = sum('Code', 'Smell')
+    t.is(result, 'CodeSmell')
+})
