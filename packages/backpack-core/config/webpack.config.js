f60242a4d6a59a1e488d71598f3f032d6d19d9e9
Jared Palmer <jared@palmer.net>
2018-08-28 09:10:17 -0400

    Upgrade to Webpack 4

--- packages/backpack-core/config/webpack.config.js
+++ packages/backpack-core/config/webpack.config.js
@@ -1,29 +1,33 @@
-const fs = require('fs')
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
-const config = require('./paths')
-const path = require('path')
-const babelPreset = require('../babel')
+const fs = require('fs');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const config = require('./paths');
+const path = require('path');
+const babelPreset = require('../babel');
 
 // This is the Webpack configuration.
 // It is focused on developer experience and fast rebuilds.
-module.exports = (options) => {
-  const babelRcPath = path.resolve('.babelrc')
-  const hasBabelRc = fs.existsSync(babelRcPath)
+module.exports = options => {
+  const babelRcPath = path.resolve('.babelrc');
+  const hasBabelRc = fs.existsSync(babelRcPath);
   const mainBabelOptions = {
     babelrc: true,
     cacheDirectory: true,
-    presets: []
-  }
+    presets: [],
+  };
 
   if (hasBabelRc) {
-    console.log('> Using .babelrc defined in your app root')
+    console.log('> Using .babelrc defined in your app root');
   } else {
-    mainBabelOptions.presets.push(require.resolve('../babel'))
+    mainBabelOptions.presets.push(require.resolve('../babel'));
   }
 
   return {
+    // Webpack v4 add a mode configuration option tells webpack to use its
+    // built-in optimizations accordingly.
+    // @see https://webpack.js.org/concepts/mode/
+    mode: options.env === 'development' ? 'development' : 'production',
     // Webpack can target multiple environments such as `node`,
     // `browser`, and even `electron`. Since Backpack is focused on Node,
     // we set the default target accordingly.
@@ -44,13 +48,13 @@ module.exports = (options) => {
         /\.(svg|png|jpg|jpeg|gif|ico|webm)$/,
         /\.(mp4|mp3|ogg|swf|webp)$/,
         /\.(css|scss|sass|less|styl)$/,
-      ]
+      ],
     }),
     // As of Webpack 2 beta, Webpack provides performance hints.
     // Since we are not targeting a browser, bundle size is not relevant.
     // Additionally, the performance hints clutter up our nice error messages.
     performance: {
-      hints: false
+      hints: false,
     },
     // Since we are wrapping our own webpack config, we need to properly resolve
     // Backpack's and the given user's node_modules without conflict.
@@ -63,12 +67,10 @@ module.exports = (options) => {
     },
     node: {
       __filename: true,
-      __dirname: true
+      __dirname: true,
     },
     entry: {
-      main: [
-        `${config.serverSrcPath}/index.js`
-      ],
+      main: [`${config.serverSrcPath}/index.js`],
     },
     // This sets the default output file path, name, and compile target
     // module type. Since we are focused on Node.js, the libraryTarget
@@ -78,7 +80,7 @@ module.exports = (options) => {
       filename: '[name].js',
       sourceMapFilename: '[name].map',
       publicPath: config.publicPath,
-      libraryTarget: 'commonjs2'
+      libraryTarget: 'commonjs2',
     },
     // Define a few default Webpack loaders. Notice the use of the new
     // Webpack 2 configuration: module.rules instead of module.loaders
@@ -88,19 +90,25 @@ module.exports = (options) => {
         // It is focused on developer experience and fast rebuilds.
         {
           test: /\.json$/,
-          loader: require.resolve('json-loader')
+          loader: require.resolve('json-loader'),
         },
         // Process JS with Babel (transpiles ES6 code into ES5 code).
         {
           test: /\.(js|jsx)$/,
           loader: require.resolve('babel-loader'),
-          exclude: [
-            /node_modules/,
-            config.buildPath
-          ],
-          options: mainBabelOptions
-        }
-      ]
+          exclude: [/node_modules/, config.buildPath],
+          options: mainBabelOptions,
+        },
+      ],
+    },
+    // A few commonly used plugins have been removed from Webpack v4.
+    // Now instead, these plugins are avaliable as "optimizations".
+    // @see https://webpack.js.org/configuration/optimization/
+    optimization: {
+      // optimization.noEmitOnErrors prevents Webpack
+      // The NoEmitOnErrorsPlugin plugin prevents Webpack
+      // from printing out compile time stats to the console.
+      noEmitOnErrors: true,
     },
     plugins: [
       // We define some sensible Webpack flags. One for the Node environment,
@@ -109,7 +117,7 @@ module.exports = (options) => {
       // want to configure __DEV__ as a global variable accordingly.
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(options.env),
-        '__DEV__': options.env === 'development'
+        __DEV__: options.env === 'development',
       }),
       // In order to provide sourcemaps, we automagically insert this at the
       // top of each file using the BannerPlugin.
@@ -118,21 +126,18 @@ module.exports = (options) => {
         entryOnly: false,
         banner: `require('${
           // Is source-map-support installed as project dependency, or linked?
-          ( require.resolve('source-map-support').indexOf(process.cwd()) === 0 )
-            // If it's resolvable from the project root, it's a project dependency.
-            ? 'source-map-support/register'
-            // It's not under the project, it's linked via lerna.
-            : require.resolve('source-map-support/register')
-        }')`
+          require.resolve('source-map-support').indexOf(process.cwd()) === 0
+            ? // If it's resolvable from the project root, it's a project dependency.
+              'source-map-support/register'
+            : // It's not under the project, it's linked via lerna.
+              require.resolve('source-map-support/register')
+        }')`,
       }),
       // The FriendlyErrorsWebpackPlugin (when combined with source-maps)
       // gives Backpack its human-readable error messages.
       new FriendlyErrorsWebpackPlugin({
         clearConsole: options.env === 'development',
       }),
-      // The NoEmitOnErrorsPlugin plugin prevents Webpack
-      // from printing out compile time stats to the console.
-      new webpack.NoEmitOnErrorsPlugin()
-    ]
-  }
-}
+    ],
+  };
+};
