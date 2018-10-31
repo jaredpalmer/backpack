71d9fecb9f0cc6afd878fd3af1128d0f6cc72402
Jared Palmer <jared@palmer.net>
2018-10-31 14:56:51 -0400

    Fix babel 7

--- packages/backpack-core/babel.js
+++ packages/backpack-core/babel.js
@@ -1,1 +1,4 @@
-module.exports = require('babel-preset-backpack')
+module.exports = function(api, opts) {
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+  return require('babel-preset-backpack')(api, opts);
+};
