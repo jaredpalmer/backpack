cb83edaed11b931bff52bf5af2913942b0252f1f
Jared Palmer <jared@palmer.net>
2018-10-31 14:57:16 -0400

    Simplify babel export

--- packages/backpack-core/babel.js
+++ packages/backpack-core/babel.js
@@ -1,4 +1,1 @@
-module.exports = function(api, opts) {
-  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-  return require('babel-preset-backpack')(api, opts);
-};
+module.exports = require('babel-preset-backpack');
