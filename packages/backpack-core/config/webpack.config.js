e092aba4cc8ac62cff24a38c8af4173230807598
Matthew Leffler <mattleff@gmail.com>
2018-11-01 11:35:07 -0600

    Remove json loader for Webpack 4 compatibility (#144)
    Refs #142.

--- packages/backpack-core/config/webpack.config.js
+++ packages/backpack-core/config/webpack.config.js
@@ -86,12 +86,6 @@ module.exports = options => {
     // Webpack 2 configuration: module.rules instead of module.loaders
     module: {
       rules: [
-        // This is the development configuration.
-        // It is focused on developer experience and fast rebuilds.
-        {
-          test: /\.json$/,
-          loader: require.resolve('json-loader'),
-        },
         // Process JS with Babel (transpiles ES6 code into ES5 code).
         {
           test: /\.(js|jsx)$/,
