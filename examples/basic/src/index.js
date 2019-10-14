0cb221d46e43148cb35deb9f9d455bb666f86803
Rocky Santiago <raphael.santiago@icloud.com>
2019-10-14 20:34:15 +0800

    update basic example

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,25 +1,24 @@
-const express = require('express')
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', async (req, res) => {
-  try {
-    const thing = await Promise.resolve({ one: 'two' }) // async/await!
-    return res.json({...thing, hello: 'world'}) // object-rest-spread!
-  } catch (e) {
-    return res.json({ error: e.message })
-  }
-})
-const port = process.env.PORT || 3000
+  const thing = await Promise.resolve({ one: 'two' }) // async/await!
+    .catch(e => res.json({ error: e.message }));
+  return res.json({ ...thing, hello: 'world' }); // object-rest-spread!
+});
 
-app.listen(port, (err) => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, err => {
   if (err) {
-    console.error(err)
+    console.error(err);
   }
 
-  if (__DEV__) { // webpack flags!
-    console.log('> in development')
+  if (__DEV__) {
+    // webpack flags!
+    console.log('> in development');
   }
 
-  console.log(`> listening on port ${port}`)
-})
+  console.log(`> listening on port ${port}`);
+});
