ca49ec6090b3e0ac0e78298f06733a5c2dda7c52
Rocky Santiago <raphael.santiago@icloud.com>
2019-10-15 01:13:36 +0800

    Refactor/update examples (#167)
    * update basic example
    
    * update with-custom-webpack-config example
    
    * fix unit test not exiting
    
    * update with-jest example

--- examples/with-jest/__tests__/createServer.test.js
+++ examples/with-jest/__tests__/createServer.test.js
@@ -1,21 +1,20 @@
 /* global it, expect, describe */
 
-import createServer from '../src/createServer'
-import request from 'supertest'
+import createServer from '../src/createServer';
+import request from 'supertest';
 
-const app = createServer()
+const app = createServer();
 
 describe('Backpack with Jest', () => {
   it('Object Rest Spread', () => {
-    const hello = { hello: 'world' }
-    const other = {...hello, nice: 'to meet you' }
+    const hello = { hello: 'world' };
+    const other = { ...hello, nice: 'to meet you' };
 
-    expect(other).toEqual({ hello: 'world', nice: 'to meet you'})
-  })
+    expect(other).toEqual({ hello: 'world', nice: 'to meet you' });
+  });
 
   it('HTTP Request', async () => {
-    const res = await request(app.listen())
-      .get('/')
-    expect(res.status).toBe(200)
-  })
-})
+    const res = await request(app).get('/');
+    expect(res.status).toBe(200);
+  });
+});
